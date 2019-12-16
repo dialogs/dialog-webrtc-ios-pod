8104b81e2925517794e504742786d764e47fbea4
Dmitriy Bagrov <d.bagrov@dlg.im>
2019-12-16 14:42:50 +0300

    feature(script): updated cdn link

--- Script/download.sh
+++ Script/download.sh
@@ -5,7 +5,7 @@ webrtc_version=79.0.29507
 sha1_checksum=68f25658e59977242b3e6f67e3c61e10fc8f7e6a
 
 echo "Fetching WebRTC(version ${webrtc_version}) archive"
-curl -OL https://cdn.dlg.im/ioswebrtc/m79/WebRTC.framework.zip
+curl -OL https://dialog-ios-cdn.s3.eu-west-2.amazonaws.com/WebRTC.framework.zip
 echo "${sha1_checksum}  WebRTC.framework.zip" | shasum -c
 unzip -o -q WebRTC.framework.zip
 rm WebRTC.framework.zip
