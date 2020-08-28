82dfcf4bb8de6b7eae0a70d61ae25d5d72c2c9a6
Anton Malygin <a.malygin@dlg.im>
2020-08-28 15:01:14 +0300

    fix download script

--- Script/download.sh
+++ Script/download.sh
@@ -3,9 +3,10 @@ set -ev
 
 webrtc_version=27.08.2020-15-07
 sha1_checksum=4872c361692d498a7768750726c1adf504660063
+filename="WebRTC-${webrtc_version}.framework.zip"
 
 echo "Fetching WebRTC(version ${webrtc_version}) archive"
-curl -OL https://dialog-ios-cdn.s3.eu-west-2.amazonaws.com/WebRTC-27.08.2020-15-07.framework.zip
-echo "${sha1_checksum}  WebRTC.framework.zip" | shasum -c
-unzip -o -q WebRTC.framework.zip
-rm WebRTC-27.08.2020-15-07.framework.zip
+curl -OL "https://dialog-ios-cdn.s3.eu-west-2.amazonaws.com/${filename}"
+echo "${sha1_checksum}  ${filename}" | shasum -c
+unzip -o -q $filename
+rm $filename
